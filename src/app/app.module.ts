12b7b37632ddc5b90dbb29a4991c2429afc28899
Adiyamseged Nerri <adiyamgn@outlook.com>
2017-03-28 13:50:07 -0400

    Added the firebase configuration and included angular fire module to the import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyBZMEmjfS8YYbzEak2AX-x7yafFoBEmSfQ",
+  authDomain: "chatapper-bedd7.firebaseapp.com",
+  databaseURL: "https://chatapper-bedd7.firebaseio.com",
+  storageBucket: "chatapper-bedd7.appspot.com",
+  messagingSenderId: "576566423365"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
